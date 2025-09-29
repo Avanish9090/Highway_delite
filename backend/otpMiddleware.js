30e9a0692d79fc69660c01efcfe91315a8bd09e6
Avanish Kumar Patel <150577239+Avanish9090@users.noreply.github.com>
2025-09-29 10:07:54 +0530

    Update otpMiddleware.js

--- backend/otpMiddleware.js
+++ backend/otpMiddleware.js
@@ -22,11 +22,13 @@ const sendOtpMiddleware = async (req, res, next) => {
 
     // Setup Nodemailer
     const transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, 
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
+           user: process.env.EMAIL_USER,
+           pass: process.env.EMAIL_PASS,
+          },
     });
 
     // Email content
